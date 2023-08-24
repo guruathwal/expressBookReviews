52c3e4b8f48e5d2247cc47b59310db3750206a05
Gurmeet Athwal <gurmeet.athwal@gmail.com>
2023-08-24 15:44:31 +0530

    Add Promise Methods

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,65 +10,100 @@ public_users.post("/register", (req, res) => {
     let password = req.body.password;
 
     if (!userName || !password) {
-      // Check for missing username or password fields
-      return res.status(400).json({ message: "Username and password are required!" });
+        // Check if missing username or password fields
+        return res.status(400).json({ message: "Username and password are required!" });
     }
 
     if (!isValid(userName)) {
-      if (password.length > 5) {
-        users.push({ "username": userName, "password": password });
-        return res.send("User Registered Successfully!");
-      } else {
-        return res.status(400).json({ message: "Password should be at least 5 characters!" });
-      }
+        // check is password is of atleast 5 characters
+        if (password.length > 5) {
+                users.push({ "username": userName, "password": password });
+                return res.send("User Registered Successfully!");
+        } else {
+                return res.status(400).json({ message: "Password should be at least 5 characters!" });
+        }
     } else {
-      return res.status(409).json({ message: "Username already exists!" });
+        return res.status(409).json({ message: "Username already exists!" });
     }
 });
 
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    return res.send(JSON.stringify({books},null,4));
+public_users.get('/',async function (req, res) {
+
+    return new Promise((resolve, reject) => {
+        if (books) {
+            return resolve( res.send(JSON.stringify({ books }, null, 4)));
+        } else {
+            return reject({ status: 404, message: `No books found.` });
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error.message);
+        const status = error.status || 500;
+        return res.status(status).json({ message: error.message });
+    });
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    let isbn = req.params.isbn; // read ISBN value
-    let maxISBN = Object.keys(books).length; // max possible ISBN value
-    if (isbn > 0 && isbn <= maxISBN){ // check if ISBN value is within limits
-        return res.send(books[isbn]);
-    } else{
-        return res.status(404).json({message: `ISBN ${isbn} does not exist`});
-    }
+public_users.get('/isbn/:isbn',async function (req, res) {
+    const isbn = req.params.isbn; // read isbn value
+
+    return new Promise((resolve, reject) => {
+        const maxISBN = Object.keys(books).length; // max possible ISBN value
+
+        if (isbn > 0 && isbn <= maxISBN) { // check if ISBN value is within limits
+            return resolve(res.send(books[isbn]));
+        } else {
+            return reject({ status: 404, message: `ISBN ${isbn} does not exist` });
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error.message);
+        const status = error.status || 500;
+        return res.status(status).json({ message: error.message });
+    });
  });
 
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    let authorName = req.params.author; // read author name string
-     // filter book by author name
-    let filteredBooks = Object.values(books).filter(book => book.author === authorName);
+public_users.get('/author/:author',async  function (req, res) {
+    const authorName = req.params.author; // read author name
 
-    // check if any book is found
-    if (filteredBooks.length > 0){
-        return res.send(filteredBooks);
-    } else{
-        return res.status(404).json({message: `Book with the Author name ${authorName} not found`});
-    }
+    return new Promise((resolve, reject) => {
+        const filteredBooks = Object.values(books).filter(book => book.author === authorName);
+
+        if (filteredBooks.length > 0) {
+            return resolve(res.send(filteredBooks));
+        } else {
+            return reject({ status: 404, message: `Book with the Author name ${authorName} not found` });
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error.message);
+        const status = error.status || 500;
+        return res.status(status).json({ message: error.message });
+    });
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    let bookTitle = req.params.title; // read title string
-     // filter book by title
-    let filteredBooks = Object.values(books).filter(book => book.title === bookTitle);
 
-    // check if any book is found
-    if (filteredBooks.length > 0){
-        return res.send(filteredBooks[0]);
-    } else{
-        return res.status(404).json({message: `Book with the Title ${bookTitle} not found`});
-    }
+// Get all books based on title
+public_users.get('/title/:title',async function (req, res) {
+    let bookTitle = req.params.title; // read title
+
+    return new Promise((resolve, reject) => {
+       let filteredBooks = Object.values(books).filter(book => book.title === bookTitle);
+
+        if (filteredBooks.length > 0) {
+            return resolve(res.send(filteredBooks));
+        } else {
+            return reject({ status: 404, message: `Book with the Title ${bookTitle} not found` });
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error.message);
+        const status = error.status || 500;
+        return res.status(status).json({ message: error.message });
+    });
 });
 
 //  Get book review
